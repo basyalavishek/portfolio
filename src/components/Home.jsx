657125dccf0b9d97f2c7a68aab822f7f0fc7310e
Anonymous-2003 <avishekbasyal3@gmail.com>
2024-12-23 19:54:20 +0545

    navbar

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import second from '../assets/heroImage.JPG'
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
