b9d5bc76d0641366a2deb8dbbdcc514d03ed2fe6
Anonymous-2003 <avishekbasyal3@gmail.com>
2024-12-24 07:10:07 +0545

    hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,10 +5,10 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 const Home = () => {
   return (
     <div name ='home' className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+        <div className='max-w-screen-lg mx-auto gap-x-20 flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>
-                    I'm a MERN Stack Developer and AI/ML enthusiast.
+                    I'm a full Stack Developer and AI/ML enthusiast.
                 </h2>
                 <p className='text-gray-400 py-4 max-w-md'>
                     about yourself
@@ -19,7 +19,7 @@ const Home = () => {
                 </div>
             </div>
             <div>
-                <img src={hero} alt="My Profile" className='rounded-2xl mx-auto w-2/3 md:w-full' />
+                <img src={hero} alt="My Profile" className=' rounded-2xl mx-auto w-2/3 md:w-full ' />
             </div>
         </div>
     </div>
